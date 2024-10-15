168d1c00d251dfcc8272c37a309c37794bfb3350
sanju <sanjayjanardhan231@gmail.com>
2024-10-15 17:30:32 +0530

    aded navbar

---
+++ e-commerce/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce/src/components/Nav.js
@@ -0,0 +1,20 @@
+import { Container, Nav } from "react-bootstrap";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navbar;
